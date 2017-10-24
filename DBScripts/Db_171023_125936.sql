7498bc2930bca85bf99c768e31d81deb1a936bec
Pooja Prajod <poojaprajod@gmail.com>
2017-10-24 12:08:24 +0200

    Revert "minor changes"
    This reverts commit 89bc057ddad179edfd09073dd2d570846cc421a0.

--- DBScripts/Db_171023_125936.sql
+++
@@ -1,8 +0,0 @@
--- Keywords_Collections [rel3]
-create table "public"."keywords_collections" (
-   "keywords_name"  varchar(255) not null,
-   "collections_oid_collection"  int4 not null,
-  primary key ("keywords_name", "collections_oid_collection")
-);
-
-
--- DBScripts/Db_171023_145446.sql
+++
@@ -1,8 +0,0 @@
--- Collections_Keywords [rel8]
-create table "public"."collections_keywords" (
-   "collections_oid_collection"  int4 not null,
-   "keywords_name"  varchar(255) not null,
-  primary key ("collections_oid_collection", "keywords_name")
-);
-
-
